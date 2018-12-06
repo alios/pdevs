d02e8b3e931b4656300d18ebd24018a2e2c5be24
Markus Barenhoff <mbarenh@alios.org>
2018-12-06 09:05:42 +0100

    use vector

--- src/Control/Pdevs/Component.hs
+++ src/Control/Pdevs/Component.hs
@@ -2,19 +2,53 @@
 {-# LANGUAGE GADTs          #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeOperators  #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Control.Pdevs.Component
-    ( ComponentPath, Component(..), Z(..)
+    ( ComponentPath, _ComponentPath, HasComponentPath(..)
+    , HasComponentPaths(..), Component(..)
+    , Z(..)
+    , RootCoordinator(..)
     ) where
 
 import           Control.Pdevs.AtomicModel
+import           Data.Typeable (Typeable)
+import           Control.Lens.Getter
+import           Control.Lens.Lens
+import           Control.Lens.Cons
+import           Control.Lens.Fold
+import           Control.Lens.Prism
+import           Control.Lens.TH
+import           Data.Vector (Vector)
 
-type ComponentPath  = [Int]
+newtype ComponentPath  =
+  ComponentPath (Vector Int)
+  deriving (Eq, Ord, Show, Semigroup, Monoid)
+
+makePrisms ''ComponentPath
+makeClassy ''ComponentPath
+
+instance Cons ComponentPath ComponentPath Int Int where
+  _Cons = prism' f t
+    where
+      f (i, (ComponentPath p)) = ComponentPath (i <| p)
+      t (ComponentPath p) = do
+        (i, p') <- preview _Cons p
+        return (i, ComponentPath p')
+
+
+class HasComponentPaths a where
+  componentPaths :: (Monoid (f ComponentPath), Applicative f) =>
+    Getter a (f ComponentPath)
 
 data Z t d x y where
   BindInput  :: Component t d x' y' -> (x -> x')  -> Z t d x y
   BindOutput :: Component t d x' y' -> (y' -> y) -> Z t d x y
   BindIntern :: Component t d x' y' -> Component t d x'' y'' -> (y' -> x'') -> Z t d x y
+  deriving (Typeable)
 
 instance Show (Z t d x y) where
   show (BindInput c _) = mconcat
@@ -24,10 +58,44 @@ instance Show (Z t d x y) where
   show (BindIntern a b _) = mconcat
     ["BindIntern (", show a, ") (", show b, ")" ]
 
+instance HasComponentPaths (Z t d x y) where
+  componentPaths = to zComponentPaths
+    where
+      zComponentPaths (BindInput c _) = view componentPaths c
+      zComponentPaths (BindOutput c _) = view componentPaths c
+      zComponentPaths (BindIntern a b _) =
+        view componentPaths a `mappend` view componentPaths b
+
 data Component t (d :: [*]) x y where
   Simulator   :: ComponentPath -> AtomicModel t s x y -> s -> Component t d x y
-  Coordinator :: ComponentPath -> [Z t (d':d) x y] -> Component t d x y
+  Coordinator :: ComponentPath -> Vector (Z t (d':d) x y) -> Component t d x y
+
+instance HasComponentPath (Component t d x y) where
+  componentPath = lens getter setter
+    where getter (Simulator p _ _ ) = p
+          getter (Coordinator p _) = p
+          setter (Simulator _ m s ) p = Simulator p m s
+          setter (Coordinator _ zs) p = Coordinator p zs
 
 instance Show (Component t d x y) where
   show (Simulator p _ _) = "Simulator " <> show p
   show (Coordinator p zs) = mconcat ["Coordinator ", show p, " ", show zs]
+
+instance HasComponentPaths (Component t d x y) where
+  componentPaths = to componentPaths'
+    where
+      componentPaths' (Simulator p _ _) = pure p
+      componentPaths' (Coordinator p zs) =
+        foldl (\b -> mappend b . view componentPaths) (pure p) zs
+
+newtype RootCoordinator t x y =
+  RootCoordinator (Vector (Z t '[] x y))
+
+instance Show (RootCoordinator t x y) where
+  show (RootCoordinator zs) = "RootCoordinator " <> show zs
+
+instance HasComponentPaths (RootCoordinator t x y) where
+  componentPaths = to componentPaths'
+    where
+      componentPaths' (RootCoordinator zs) =
+        foldl (\b -> mappend b . view componentPaths) mempty zs
--- src/Control/Pdevs/Coupled.hs
+++ src/Control/Pdevs/Coupled.hs
@@ -10,6 +10,7 @@ module Control.Pdevs.Coupled
     ( module Control.Pdevs.Coupled.Class
     , CoupledT, mkCoordinator
     , RootCoordinator, mkRootCoordinator
+    , ComponentPath, HasComponentPaths(..)
     ) where
 
 import           Control.Lens.Getter
@@ -20,6 +21,7 @@ import           Control.Monad.State
 import           Control.Monad.Writer
 import           Control.Pdevs.Component
 import           Control.Pdevs.Coupled.Class
+import           Data.Vector (Vector)
 
 data CoupledState = CoupledState
     { _coupledStateI    :: Int
@@ -28,9 +30,9 @@ data CoupledState = CoupledState
 makeClassy ''CoupledState
 
 newtype CoupledT t (d :: [*]) x y m a =
-  CoupledT (StateT CoupledState (WriterT [Z t d x y] m) a)
+  CoupledT (StateT CoupledState (WriterT (Vector (Z t d x y)) m) a)
   deriving ( Functor, Applicative, Monad
-           , MonadState CoupledState, MonadWriter [Z t d x y]
+           , MonadState CoupledState, MonadWriter (Vector (Z t d x y))
            )
 
 _CoupledState :: Iso' CoupledState ComponentPath
@@ -40,20 +42,14 @@ instance (Monad m) => MonadCoupled (CoupledT t d x y m) t d x y where
   nextComponentPath = do
     i <- use coupledStateI
     coupledStateI += 1
-    (:) <$> pure i <*> use coupledStatePath
+    (<|) <$> pure i <*> use coupledStatePath
   addBinding = tell . pure
 
 instance MonadTrans (CoupledT t d x y) where
   lift = CoupledT . lift . lift
 
-runCoupledT :: Monad m => ComponentPath -> CoupledT t d x y m a -> m [Z t d x y]
+runCoupledT :: Monad m => ComponentPath -> CoupledT t d x y m a -> m (Vector (Z t d x y))
 runCoupledT p (CoupledT m) = snd <$> runWriterT (evalStateT m (_CoupledState # p))
-
-newtype RootCoordinator t x y =
-  RootCoordinator [Z t '[] x y]
-
-instance Show (RootCoordinator t x y) where
-  show (RootCoordinator zs) = "RootCoordinator " <> show zs
 
 mkCoordinator :: Monad m => CoupledT t (d' : d) x' y' m () -> CoupledT t d x y m (Component t d x' y')
 mkCoordinator m = do
@@ -64,7 +60,7 @@ mkCoordinator m = do
 mkRootCoordinator
   :: Monad m
   => CoupledT t '[] x y m (Component t '[] x y) -> m (RootCoordinator t x y)
-mkRootCoordinator m = fmap RootCoordinator . runCoupledT [] $ do
+mkRootCoordinator m = fmap RootCoordinator . runCoupledT mempty $ do
   rc <- m
   bindInput rc id
   bindOutput rc id
